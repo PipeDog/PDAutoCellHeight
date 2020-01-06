b5334ae3016a39ba3239b32a1a38ca14ab5601ae
liang <18811130435@163.com>
2020-01-06 17:27:40 +0800

    commit

--- PDAutoCellHeight/PDDynamicHeightCell.m
+++ PDAutoCellHeight/PDDynamicHeightCell.m
@@ -17,6 +17,7 @@ static CGFloat const kAroundMargin = 10;
 @interface PDDynamicHeightCell ()
 
 @property (nonatomic, strong) UILabel *contentLabel;
+@property (nonatomic, strong) UILabel *bottomLabel;
 
 @end
 
@@ -25,8 +26,8 @@ static CGFloat const kAroundMargin = 10;
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-        // self.pd_bottomView = self.contentLabel;
-        self.pd_bottomViews = @[self.contentLabel];
+         self.pd_bottomView = self.bottomLabel;
+//        self.pd_bottomViews = @[self.contentLabel];
         self.pd_bottomOffset = kAroundMargin;
         self.pd_enforceFrameLayout = YES;
         
@@ -38,13 +39,24 @@ static CGFloat const kAroundMargin = 10;
 
 - (void)createViewHierarchy {
     [self.contentView addSubview:self.contentLabel];
+    [self.contentView addSubview:self.bottomLabel];
 }
 
 - (void)layoutContentViews {
     [self.contentLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.mas_equalTo(kAroundMargin);
         make.left.mas_equalTo(kAroundMargin);
+        
+        // You should set const width for `label`
         make.width.mas_equalTo(kScreenWidth - kAroundMargin * 2);
+        
+        // Don't do like the follow code
+        // make.width.equalTo(self.contentView.mas_width).offset(-kAroundMargin * 2);
+    }];
+    
+    [self.bottomLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.contentLabel.mas_bottom).offset(kAroundMargin);
+        make.left.and.right.equalTo(self.contentLabel);
     }];
 }
 
@@ -52,12 +64,15 @@ static CGFloat const kAroundMargin = 10;
 - (void)loadData:(NSString *)text {
     _contentLabel.text = text;
     [_contentLabel sizeToFit];
+    
+    _bottomLabel.text = text;
+    [_bottomLabel sizeToFit];
 }
 
 #pragma mark - Getter Methods
 - (UILabel *)contentLabel {
     if (!_contentLabel) {
-        _contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(kAroundMargin, kAroundMargin, kScreenWidth - kAroundMargin * 2, 0)];
+        _contentLabel = [[UILabel alloc] init];
         _contentLabel.backgroundColor = [UIColor lightGrayColor];
         _contentLabel.textColor = [UIColor darkTextColor];
         _contentLabel.font = [UIFont systemFontOfSize:13];
@@ -65,6 +80,18 @@ static CGFloat const kAroundMargin = 10;
         _contentLabel.textAlignment = NSTextAlignmentLeft;
     }
     return _contentLabel;
+}
+
+- (UILabel *)bottomLabel {
+    if (!_bottomLabel) {
+        _bottomLabel = [[UILabel alloc] init];
+        _bottomLabel.backgroundColor = [UIColor lightGrayColor];
+        _bottomLabel.textColor = [UIColor darkTextColor];
+        _bottomLabel.font = [UIFont systemFontOfSize:13];
+        _bottomLabel.numberOfLines = 0;
+        _bottomLabel.textAlignment = NSTextAlignmentLeft;
+    }
+    return _bottomLabel;
 }
 
 @end
